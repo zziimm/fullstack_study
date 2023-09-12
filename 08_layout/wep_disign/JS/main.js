71147d24384b5e2b63e9bc6bfd5f55930c313a3a
zziimm <wlalscjs30@naver.com>
2023-09-12 20:53:32 +0900

    ee

--- 08_layout/wep_disign/JS/main.js
+++ 08_layout/wep_disign/JS/main.js
@@ -6,7 +6,7 @@ console.log(slides);
 
 // 슬라이드 쇼에 있는 이미지 3개 중에서 하나가 표시되면 나머지 이미지는 모두 감춤
 // 이전 또는 다음 버튼을 클릭하면 이전 이미지나 다음 이미지를 보여줌
-// 슬라이드 번호를 저장할 변수 currnent 선언
+// 슬라이드 번호를 저장할 변수 current 선언
 let current = 0;
 
 // corrent 값에 따라 현재 이미지를 보여주는 showSlides() 함수
@@ -52,3 +52,22 @@ function nextSlide() {
 showSlides(current);  // 현재 이미지를 표시
 prev.addEventListener('click', prevSlide);  // 이전 이미지 표시
 next.addEventListener('click', nextSlide);  // 다음 이미지 표시
+
+// (참고)
+// 이미지 슬라이드 쇼를 자동으로 바꾸는 방법
+// const slides = document.querySelectorAll('#slides > img');
+// let current = 0;
+
+// function showSlides() {
+//   slides.forEach(function (slideEl) {
+//     slideEl.style.display = 'none'; // 모든 이미지 감춤
+//   });
+//   current++; // 다음 이미지로 이동
+//   if (current > slides.length) { // 마지막 이미지라면
+//     current = 1; // 첫 번째로 이동
+//   }
+//   slides[current - 1].style.display = "block"; // 현재 위치 이미지 표시
+//   setTimeout(showSlides, 2000); // 2초마다 showSlides 함수 반복 실행 
+// }
+
+// showSlides(); // 기본적으로 첫 번째 이미지 표시
